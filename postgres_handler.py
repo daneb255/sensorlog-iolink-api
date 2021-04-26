39ec3e2b9452993b70ea75f8c9ea09e3b901a42f
Daniel Bitzer <d@bitzer.io>
2021-04-26 12:07:38 +0200

    fix postgres

--- postgres_handler.py
+++ postgres_handler.py
@@ -10,9 +10,10 @@ def write_to_postgres(timestamp, x, y, z, device_id, label):
                                 password=POSTGRESPASSWORD,
                                 database=POSTGRES_DB)
         cursor = conn.cursor()
-        cursor.execute("INSERT INTO motionUserAcceleration (device_id, label, date_time, motionUserAccelerationX,"
-                       " motionUserAccelerationY, motionUserAccelerationZ) VALUES(%s, %s, %s, %s, %s, %s)",
+        cursor.execute("INSERT INTO motionuseracceleration (device_id, label, date_time, motionuseraccelerationx, motionuseraccelerationy,"
+                       " motionuseraccelerationz) VALUES(%s, %s, %s, %s, %s, %s)",
                        (device_id, label, timestamp, x, y, z))
+        conn.commit()
         cursor.close()
         conn.close()
         return True
